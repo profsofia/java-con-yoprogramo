33121160cd60b47acbd9fdb91051323ca50e890a
Sofía Schenone <sofiainesschenone@gmail.com>
2022-01-18 18:44:56 -0300

    se agregaron dos métodos a la clase principal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola mundo");
+        method_of_no_return("Sofi");
+        System.out.println("Encontremos el mínimo entre dos números: " + min(5,9));
+    }
+    public static int min(int a, int b) {
+        //retorna el minimo entre dos numeros enteros
+        int minimum;
+        if (a>b)
+          minimum=b;
+        else
+          minimum=a;
+        return minimum;
+    }
+    //la palabra clave void se usa para métodos que no tienen retorno
+    public static void method_of_no_return(String nombre) {
+        System.out.println("Hola "+ nombre );
     }
 }
+
